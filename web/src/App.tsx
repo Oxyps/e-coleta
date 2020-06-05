acf02a0a194866b4835f504e4f39612ccd9faeda
Oxyps <abel_castro_junior@hotmail.com>
2020-06-05 00:17:59 -0300

    web points nd home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './App.css';
+
+import Routes from './routes';
+
 function App() {
   return (
-    <div className="App">
-      <h1>READY</h1>
-    </div>
+	<div>
+		<Routes />
+	</div>
   );
 }
 
---
+++ web/src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
+export default function Routes() {
+	return(
+		<BrowserRouter>
+			<Route exact component={Home} path='/' />
+			<Route component={CreatePoint} path='/create-point' />
+		</BrowserRouter>
+	);
+}
